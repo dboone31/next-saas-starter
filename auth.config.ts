4ff192c5446952bf7443f35c8a370dba18731674
David Boone Jr <dboone31@users.noreply.github.com>
2024-04-10 08:12:08 -0400

    removing email auth as it doesn't work with vercel middleware

--- auth.config.ts
+++ auth.config.ts
@@ -1,21 +1,22 @@
-// import Google from "next-auth/providers/google"
-// import { env } from "@/env.mjs"
+import Google from "next-auth/providers/google"
+import { env } from "@/env.mjs"
 
-import type { NextAuthConfig } from "next-auth"
-import { sendVerificationRequest } from "./utils/send-verification-reqest"
-import Nodemailer from "@auth/core/providers/nodemailer"
+import type { NextAuthConfig } from "next-auth";
+import EmailProvider from "next-auth/providers/resend";
+
+import { sendVerificationRequest } from "./utils/send-verification-reqest";
 
 export default {
   providers: [
-    // Google({
-    //   clientId: env.GOOGLE_CLIENT_ID,
-    //   clientSecret: env.GOOGLE_CLIENT_SECRET,
+    Google({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+    // EmailProvider({
+    //   name: "email",
+    //   server: "",
+    //   from: "david@blendedcourse.com",
+    //   sendVerificationRequest,
     // }),
-    Nodemailer({
-      name: 'email',
-      server: '',
-      from: 'david@blendedcourse.com',
-      sendVerificationRequest,
-    })
   ],
-} satisfies NextAuthConfig
+} satisfies NextAuthConfig;
